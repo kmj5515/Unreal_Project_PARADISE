4f31181fb5721f11ae4449963a4cea184f193b90
김민준 <79951019+kmj5515@users.noreply.github.com>
2026-03-15 20:56:30 +0900

    어빌리티시스템 어트리뷰트 추가

---
+++ Paradise/Source/Paradise/Public/AbilitySystem/ParadiseAbilitySystemComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "ParadiseAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARADISE_API UParadiseAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+
+public:
+
+};
