146564edeb86adbc814101fcd7ba9e456c1f142a
김민준 <79951019+kmj5515@users.noreply.github.com>
2026-03-15 21:42:14 +0900

    Update Paradise.Build.cs

--- Paradise/Source/Paradise/Paradise.Build.cs
+++ Paradise/Source/Paradise/Paradise.Build.cs
@@ -8,7 +8,7 @@ public class Paradise : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "GameplayTasks", "AnimGraphRuntime", "MotionWarping", "Niagara", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks", "AnimGraphRuntime", "MotionWarping", "Niagara", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
