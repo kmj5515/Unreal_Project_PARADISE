ed42ca7f183eeb42562fcef15511ee3c790c85e8
김민준 <79951019+kmj5515@users.noreply.github.com>
2026-03-17 01:03:46 +0900

    기본 이동 로직 추가

--- Paradise/Source/Paradise/Private/Characters/ParadiseSurvivalCharacter.cpp
+++ Paradise/Source/Paradise/Private/Characters/ParadiseSurvivalCharacter.cpp
@@ -1,5 +1,109 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Characters/ParadiseSurvivalCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "GameFramework/PlayerController.h"
+
+void AParadiseSurvivalCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LocalPlayer = PC->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				if (DefaultMappingContext)
+				{
+					Subsystem->AddMappingContext(DefaultMappingContext, 0);
+				}
+			}
+		}
+	}
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_Look)
+		{
+			EnhancedInput->BindAction(IA_Look, ETriggerEvent::Triggered, this, &AParadiseSurvivalCharacter::Input_Look);
+		}
+
+		if (IA_Move)
+		{
+			EnhancedInput->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AParadiseSurvivalCharacter::Input_Move);
+		}
+
+		if (IA_Block)
+		{
+			EnhancedInput->BindAction(IA_Block, ETriggerEvent::Started, this, &AParadiseSurvivalCharacter::Input_Block_Pressed);
+			EnhancedInput->BindAction(IA_Block, ETriggerEvent::Completed, this, &AParadiseSurvivalCharacter::Input_Block_Released);
+		}
+
+		if (IA_Roll)
+		{
+			EnhancedInput->BindAction(IA_Roll, ETriggerEvent::Triggered, this, &AParadiseSurvivalCharacter::Input_Roll);
+		}
+
+		if (IA_Jump)
+		{
+			EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Started, this, &AParadiseSurvivalCharacter::Input_Jump_Pressed);
+			EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Completed, this, &AParadiseSurvivalCharacter::Input_Jump_Released);
+		}
+	}
+}
+
+void AParadiseSurvivalCharacter::Input_Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxis = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookAxis.X);
+	AddControllerPitchInput(LookAxis.Y);
+}
+
+void AParadiseSurvivalCharacter::Input_Move(const FInputActionValue& Value)
+{
+	const FVector2D MoveAxis = Value.Get<FVector2D>();
+
+	if (Controller == nullptr)
+	{
+		return;
+	}
+
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+	const FVector ForwardDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDir, MoveAxis.Y);
+	AddMovementInput(RightDir, MoveAxis.X);
+}
+
+void AParadiseSurvivalCharacter::Input_Block_Pressed(const FInputActionValue& Value)
+{
+	// TODO: Block 활성화 로직
+}
+
+void AParadiseSurvivalCharacter::Input_Block_Released(const FInputActionValue& Value)
+{
+	// TODO: Block 비활성화 로직
+}
+
+void AParadiseSurvivalCharacter::Input_Roll(const FInputActionValue& Value)
+{
+	// TODO: 구르기(Roll) 로직
+}
+
+void AParadiseSurvivalCharacter::Input_Jump_Pressed(const FInputActionValue& Value)
+{
+	Jump();
+}
+
+void AParadiseSurvivalCharacter::Input_Jump_Released(const FInputActionValue& Value)
+{
+	StopJumping();
+}
 
--- Paradise/Source/Paradise/Public/Characters/ParadiseSurvivalCharacter.h
+++ Paradise/Source/Paradise/Public/Characters/ParadiseSurvivalCharacter.h
@@ -1,10 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Characters/ParadiseCharacterBase.h"
 #include "ParadiseSurvivalCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  * 
@@ -14,4 +16,35 @@ class PARADISE_API AParadiseSurvivalCharacter : public AParadiseCharacterBase
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Enhanced Input: Mapping Context & Actions (BP에서 세팅)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputAction* IA_Look;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputAction* IA_Move;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputAction* IA_Block;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputAction* IA_Roll;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputAction* IA_Jump;
+
+protected:
+	void Input_Look(const FInputActionValue& Value);
+	void Input_Move(const FInputActionValue& Value);
+	void Input_Block_Pressed(const FInputActionValue& Value);
+	void Input_Block_Released(const FInputActionValue& Value);
+	void Input_Roll(const FInputActionValue& Value);
+	void Input_Jump_Pressed(const FInputActionValue& Value);
+	void Input_Jump_Released(const FInputActionValue& Value);
 };
